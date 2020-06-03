0ebc38290f7502709a4b8226da0b945f03d86df8
Javier Aranda <refugiero@hotmail.com>
2020-06-03 12:06:35 -0300

    e

--- contactform/contact_mail.php
+++ contactform/contact_mail.php
@@ -2,7 +2,7 @@
 $toEmail = "javier.aranda.javier@gmail.com";
 $mailHeaders = "From: " . $_POST["email"]."\r\n";
 if(mail($toEmail, $_POST["subject"], $_POST["message"], $mailHeaders)) {
-    echo "OK";
+    
 } else {
     echo"<p class='Error'>Problem in Sending Mail.</p>";
 }
